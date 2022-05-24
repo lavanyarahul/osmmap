75b238603a57921e8747f577f370f11a1110cb37
Lavanya Rahul <93192550+lavanyarahul@users.noreply.github.com>
2022-05-24 10:49:20 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,57 +1,19 @@
  <?php
+
+ require 'map.php';
+    $map = new map; 
  if(isset($_POST['address'])) 
   { 
      $address = explode(',',$_POST['address']);
      $search_url = "https://nominatim.openstreetmap.org/search?q=".$address[0]."&format=json"; 
-       $latLong = getOSMLatLong($search_url);
-       echo json_encode($latLong);
-  }  
-
-function getOSMLatLong($search_url){
-   $httpOptions = [
-      "http" => [
-          "method" => "GET",
-          "header" => "User-Agent: Nominatim-Test"
-      ]
-   ];
-
-    $streamContext = stream_context_create($httpOptions);
-    $json = file_get_contents($search_url, false, $streamContext); 
-    $decoded = json_decode($json, true);
-     
-    $data['latitude'] = $decoded[0]["lat"];
-    $data['longitude'] = $decoded[0]["lon"];
-    if(!empty($data)){
-      return $data;
-    }else{
-      return false;
-    }
-     
-}
-
- if(isset($_POST['map_address'])) 
-  { 
-     $address = explode(',',$_POST['map_address']);
-        $latLong = getMapLatLong($address);
+       $latLong = $map->getOSMLatLong($search_url);
        echo json_encode($latLong);
   } 
-function getMapLatLong($address){
-    if(!empty($address)){
-       //Formatted address
-        $formattedAddr = str_replace(' ','+',$address);
-        //Send request and receive json data by address
-        $geocodeFromAddr = file_get_contents('https://maps.googleapis.com/maps/api/js?address='.$formattedAddr.'&sensor=false&region=India&callback=myMap');
-        $output = json_decode($geocodeFromAddr);
-        //Get latitude and longitute from json data
-        $data['latitude'] = $output->results[0]->geometry->location->lat;
-        $data['longitude'] = $output->results[0]->geometry->location->lng;
-        //Return latitude and longitude of the given address
-        if(!empty($data)){
-          return $data;
-        }else{
-          return false;
-        }
-    }else{
-        return false;
-    }
-}
+
+  if(isset($_POST['map_address'])) 
+  { 
+     $address = explode(',',$_POST['map_address']);
+        $latLong = $map->getMapLatLong($address);
+       echo json_encode($latLong);
+  }
+  ?> 
